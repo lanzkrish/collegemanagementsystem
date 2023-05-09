a0d83d46c5a08c70cf35bfe60cc3f882586e48d0
Dhananjay Sahoo <Kanha94377@gmail.com>
2023-05-09 13:05:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+
+	<?php include('common/header.php') ?>
+	<div class="home-div"></div>
+		<div class="w-100 in-ad-ap">
+			<div class="row m-auto text-center">
+				<div class="col-md-4"><h3>INTRODUCING-ICBS</h3></div>
+				<div class="col-md-4"><h3>ADMISSION POLICY</h3></div>
+				<div class="col-md-4"><h3>APPLY NOW</h3></div>
+			</div>
+		</div>
+		<div class="paragraph">
+			<p>
+				This project is created by ozensoftwares.
+			</p>
+		</div>
+	<?php include('common/cards.php') ?>
+	<?php include('common/footer.php') ?>
+</body>
+</html>
+
